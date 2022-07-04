38d1905abd700899793fdb6ce55f85dd67ebbf9c
felipe.borges <trc.felipe.borges@trc.sebraeba.com.br>
2022-07-04 16:48:02 -0300

    Adicionando objeto Aluno e funcionalidade de retorno de informações na tela.

--- app/src/main/java/com/example/estudos_android_studio/ui/activity/FormularioAlunoActivity.java
+++ app/src/main/java/com/example/estudos_android_studio/ui/activity/FormularioAlunoActivity.java
@@ -8,6 +8,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.estudos_android_studio.R;
+import com.example.estudos_android_studio.model.Aluno;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
 
@@ -18,17 +19,32 @@ public class FormularioAlunoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_formulario_aluno);
 
         // Fazendo o tacking das view fornecidas pela interface visual
-        EditText campoEmail = findViewById(R.id.activity_formulario_aluno_email);
-        EditText campoNome = findViewById(R.id.activity_formulario_aluno_nome);
-        EditText campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+        // Importante na utilização de classes anônimas deixar os valores como final, caso contrario,
+        // não havera compilação
+        final EditText campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+        final EditText campoEmail = findViewById(R.id.activity_formulario_aluno_email);
+        final EditText campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+
 
         // Setando ação executada ao clicar no botão
+        // Chamando classe anônima
         View botaoSalvar = findViewById(R.id.activity_formulario_aluno_butao_salvar);
         botaoSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+             // Pegando os valores das edittext e atribuindo a variavaveis
+             String nome = campoNome.getText().toString();
+             String email = campoEmail.getText().toString();
+             String telefone = campoTelefone.getText().toString();
+
+             // Criando um objeto para armazenar as informações
+             Aluno alunoCriado = new Aluno (nome, telefone, email);
+
+             // Mensagem de teste para visualização de informações na tela
                 Toast.makeText(FormularioAlunoActivity.this,
-                        "Click no botão salvar!",
+                        alunoCriado.getNome() +
+                        " - " + alunoCriado.getEmail() + " - " +
+                        alunoCriado.getTelefone(),
                         Toast.LENGTH_LONG).show();
             }
         });
