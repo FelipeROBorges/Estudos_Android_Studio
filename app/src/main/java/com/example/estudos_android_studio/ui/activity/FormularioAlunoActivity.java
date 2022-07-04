0a89483c23b92ffe33e7d0d22c6881484ac457d7
felipe.borges <trc.felipe.borges@trc.sebraeba.com.br>
2022-07-04 16:20:32 -0300

    Adicionando busca de views por id e adicionando teste para visualização.

--- app/src/main/java/com/example/estudos_android_studio/ui/activity/FormularioAlunoActivity.java
+++ app/src/main/java/com/example/estudos_android_studio/ui/activity/FormularioAlunoActivity.java
@@ -3,6 +3,9 @@ package com.example.estudos_android_studio.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.estudos_android_studio.R;
 
@@ -13,5 +16,22 @@ public class FormularioAlunoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
+
+        // Fazendo o tacking das view fornecidas pela interface visual
+        EditText campoEmail = findViewById(R.id.activity_formulario_aluno_email);
+        EditText campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+        EditText campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+
+        // Setando ação executada ao clicar no botão
+        View botaoSalvar = findViewById(R.id.activity_formulario_aluno_butao_salvar);
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(FormularioAlunoActivity.this,
+                        "Click no botão salvar!",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
