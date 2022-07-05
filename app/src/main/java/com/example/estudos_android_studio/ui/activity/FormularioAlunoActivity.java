b924ea48eeb1205cd0e6043123e18ad7cbe50b9b
Felipe Borges <feliperoborges@gmail.com>
2022-07-04 22:32:54 -0300

    Otimização de metodos e limpeza de código.

--- app/src/main/java/com/example/estudos_android_studio/ui/activity/FormularioAlunoActivity.java
+++ app/src/main/java/com/example/estudos_android_studio/ui/activity/FormularioAlunoActivity.java
@@ -1,57 +1,57 @@
 package com.example.estudos_android_studio.ui.activity;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
-
 import com.example.estudos_android_studio.R;
 import com.example.estudos_android_studio.model.Aluno;
 import com.example.estudos_android_studio.model.AlunoDAO;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
 
+    public static final String TITULO_APPBAR = "Novo Aluno";
+    AlunoDAO dao = new AlunoDAO();
+    private EditText campoNome;
+    private EditText campoEmail;
+    private EditText campoTelefone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
+        setTitle(TITULO_APPBAR);
+        inicializaCampos();
+        salvaAluno();
 
+    }
 
-        AlunoDAO dao = new AlunoDAO();
-
-        // Fazendo o tacking das view fornecidas pela interface visual
-        // Importante na utilização de classes anônimas deixar os valores como final, caso contrario,
-        // não havera compilação
-        final EditText campoNome = findViewById(R.id.activity_formulario_aluno_nome);
-        final EditText campoEmail = findViewById(R.id.activity_formulario_aluno_email);
-        final EditText campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
-
-
-        // Setando ação executada ao clicar no botão
-        // Chamando classe anônima
+    private void salvaAluno() {
         View botaoSalvar = findViewById(R.id.activity_formulario_aluno_butao_salvar);
         botaoSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-             // Pegando os valores das edittext e atribuindo a variavaveis
-             String nome = campoNome.getText().toString();
-             String email = campoEmail.getText().toString();
-             String telefone = campoTelefone.getText().toString();
-
-             // Criando um objeto para armazenar as informações
-             Aluno alunoCriado = new Aluno (nome, telefone, email);
-
-            // Chamando metodo de salvamento de Aluno
+            Aluno alunoCriado = getAluno();
             dao.salva(alunoCriado);
-
             finish();
-
             }
         });
+    }
+
+    private void inicializaCampos() {
+        campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+        campoEmail = findViewById(R.id.activity_formulario_aluno_email);
+        campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+    }
+
+    @NonNull
+    private Aluno getAluno() {
+        String nome = campoNome.getText().toString();
+        String email = campoEmail.getText().toString();
+        String telefone = campoTelefone.getText().toString();
+        Aluno alunoCriado = new Aluno (nome, telefone, email);
+        return alunoCriado;
 
     }
+
 }
--- app/src/main/java/com/example/estudos_android_studio/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/com/example/estudos_android_studio/ui/activity/ListaAlunosActivity.java
@@ -1,59 +1,55 @@
 package com.example.estudos_android_studio.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
-
 import com.example.estudos_android_studio.R;
 import com.example.estudos_android_studio.model.AlunoDAO;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class ListaAlunosActivity extends AppCompatActivity {
+
+    public static final String TITLE_APPBAR = "Lista de Alunos";
+    private final AlunoDAO dao = new AlunoDAO();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Fazendo o link com um arquivo DML de visualização
         setContentView(R.layout.activity_lista_alunos);
-
-        // Definindo o nome exibido na parte superior da tela
-        setTitle("Lista de Alunos");
-
-        // Mensagem de entrada do app
+        setTitle(TITLE_APPBAR);
         Toast.makeText(this, "Bem-vindo!", Toast.LENGTH_LONG).show();
+        configuraFabNovoAluno();
+    }
 
-        // Direcionando o usuário para a tela de cadastro
+    private void configuraFabNovoAluno() {
         View botaoNovoAluno = findViewById(R.id.activity_lista_alunos_fab_novo_aluno);
         botaoNovoAluno.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                startActivity(new Intent(ListaAlunosActivity.this,
-                        FormularioAlunoActivity.class));
+                abreFormularioAluno();
             }
         });
+    }
 
-
+    private void abreFormularioAluno() {
+        startActivity(new Intent(this,
+                FormularioAlunoActivity.class));
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        configuraAluno();
+    }
 
-        // Instanciando classe DAO
-        AlunoDAO dao = new AlunoDAO();
-
-        // Implementando lista de alunos com classe DAO
+    private void configuraAluno() {
         ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_lista_de_alunos);
         listaDeAlunos.setAdapter(new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1,
                 dao.todos()));
     }
+
 }
